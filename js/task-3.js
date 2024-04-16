bf0843941974bbb05a327869d0dce5a519453d09
Katrin <kateyka1410@gmail.com>
2024-04-16 14:09:54 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,14 @@
-function getElementWidth(content, padding, border) {
-    const contentWidt = Number.parseFloat(content); 
-    const paddingWidth = Number.parseFloat(padding);
-    const borderWidth = Number.parseFloat(border);
-    const getElementWidt = contentWidt + 2 * paddingWidth + 2 * borderWidth;
-    return getElementWidt
+function filterArray(numbers, value) {
+    let filtArray = [];
+    for (let number of numbers) {
+        if (number > value) {
+            filtArray.push(number)
+        }
+    }
+    return filtArray
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+  console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+console.log(filterArray([12, 24, 8, 41, 76], 38)); 
+console.log(filterArray([12, 24, 8, 41, 76], 20));
